e579e6ba9bc9d9b50d8ac76a45b41f6127feb6d8
Juan Ardissone (micayael) <micayael@gmail.com>
2021-11-30 13:03:37 -0300

    Se corrige la injección de dependencias

--- src/Service/ParametroService.php
+++ src/Service/ParametroService.php
@@ -3,6 +3,7 @@
 namespace ConfigManager\Bundle\ClientBundle\Service;
 
 use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ParametroService
@@ -10,7 +11,7 @@ class ParametroService
     public const KEY = 'config_manager_configs';
 
     /** @required */
-    public ItemInterface $cache;
+    public TagAwareCacheInterface $cache;
 
     /** @required */
     public HttpClientInterface $httpClient;
